967b19266f2cc49db95d82cadd320f87a0da4fa6
01212 <marlonwiprud1@gmail.com>
2019-01-11 15:24:44 -0500

    set up bundler in server.js

--- ForeignApp.js
+++ ForeignApp.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _typeof2(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof2 = function _typeof2(obj) { return typeof obj; }; } else { _typeof2 = function _typeof2(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof2(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -13,25 +15,94 @@ var _one = _interopRequireDefault(require("./one"));
 
 var _two = _interopRequireDefault(require("./two"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : {
+    default: obj
+  };
+}
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _typeof(obj) {
+  if (typeof Symbol === "function" && _typeof2(Symbol.iterator) === "symbol") {
+    _typeof = function _typeof(obj) {
+      return _typeof2(obj);
+    };
+  } else {
+    _typeof = function _typeof(obj) {
+      return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : _typeof2(obj);
+    };
+  }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+  return _typeof(obj);
+}
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+function _possibleConstructorReturn(self, call) {
+  if (call && (_typeof(call) === "object" || typeof call === "function")) {
+    return call;
+  }
 
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+  return _assertThisInitialized(self);
+}
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+function _assertThisInitialized(self) {
+  if (self === void 0) {
+    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+  }
 
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+  return self;
+}
+
+function _getPrototypeOf(o) {
+  _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
+    return o.__proto__ || Object.getPrototypeOf(o);
+  };
+  return _getPrototypeOf(o);
+}
+
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function");
+  }
+
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      writable: true,
+      configurable: true
+    }
+  });
+  if (superClass) _setPrototypeOf(subClass, superClass);
+}
+
+function _setPrototypeOf(o, p) {
+  _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
+    o.__proto__ = p;
+    return o;
+  };
+
+  return _setPrototypeOf(o, p);
+}
 
 var App =
 /*#__PURE__*/
@@ -63,5 +134,339 @@ function (_React$Component) {
 }(_react.default.Component);
 
 var _default = App;
+exports.default = _default;
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+var _reactRouterDom = require("react-router-dom");
+
+var _App = _interopRequireDefault(require("./App"));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : {
+    default: obj
+  };
+} // import './styles.css';
+// import StyledApp from './styled.js'
+// import {
+//     StyledApp as App
+// } from './styled.js'
+// const App = () => {
+//   return <div className = "title">Hello React!</div>;
+// };
+
+
+_reactDom.default.render(_react.default.createElement(_reactRouterDom.BrowserRouter, null, _react.default.createElement(_App.default, null)), document.getElementById("root"));
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : {
+    default: obj
+  };
+}
+
+function _typeof(obj) {
+  if (typeof Symbol === "function" && _typeof2(Symbol.iterator) === "symbol") {
+    _typeof = function _typeof(obj) {
+      return _typeof2(obj);
+    };
+  } else {
+    _typeof = function _typeof(obj) {
+      return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : _typeof2(obj);
+    };
+  }
+
+  return _typeof(obj);
+}
+
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
+function _possibleConstructorReturn(self, call) {
+  if (call && (_typeof(call) === "object" || typeof call === "function")) {
+    return call;
+  }
+
+  return _assertThisInitialized(self);
+}
+
+function _assertThisInitialized(self) {
+  if (self === void 0) {
+    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+  }
+
+  return self;
+}
+
+function _getPrototypeOf(o) {
+  _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
+    return o.__proto__ || Object.getPrototypeOf(o);
+  };
+  return _getPrototypeOf(o);
+}
+
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function");
+  }
+
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      writable: true,
+      configurable: true
+    }
+  });
+  if (superClass) _setPrototypeOf(subClass, superClass);
+}
+
+function _setPrototypeOf(o, p) {
+  _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
+    o.__proto__ = p;
+    return o;
+  };
+
+  return _setPrototypeOf(o, p);
+}
+
+var One =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(One, _React$Component);
+
+  function One() {
+    var _this;
+
+    _classCallCheck(this, One);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(One).call(this));
+    _this.state = {};
+    return _this;
+  }
+
+  _createClass(One, [{
+    key: "handleClick",
+    value: function handleClick() {
+      console.log('CLICKED');
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      return _react.default.createElement("div", {
+        className: "one"
+      }, _react.default.createElement("h1", null, "PAGE ONE"), _react.default.createElement("div", {
+        className: "box"
+      }), _react.default.createElement("button", {
+        onClick: function onClick() {
+          return _this2.handleClick();
+        }
+      }));
+    }
+  }]);
+
+  return One;
+}(_react.default.Component);
+
+var _default = One;
+exports.default = _default;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.StyledApp = void 0;
+
+var _styledComponents = _interopRequireWildcard(require("styled-components"));
+
+var _App = _interopRequireDefault(require("./App.js"));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : {
+    default: obj
+  };
+}
+
+function _interopRequireWildcard(obj) {
+  if (obj && obj.__esModule) {
+    return obj;
+  } else {
+    var newObj = {};
+
+    if (obj != null) {
+      for (var key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+          var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {};
+
+          if (desc.get || desc.set) {
+            Object.defineProperty(newObj, key, desc);
+          } else {
+            newObj[key] = obj[key];
+          }
+        }
+      }
+    }
+
+    newObj.default = obj;
+    return newObj;
+  }
+}
+
+var StyledApp = (0, _styledComponents.default)(_App.default).withConfig({
+  displayName: "styled__StyledApp",
+  componentId: "sc-1oj3kr1-0"
+})(["color:red;"]);
+exports.StyledApp = StyledApp;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : {
+    default: obj
+  };
+}
+
+function _typeof(obj) {
+  if (typeof Symbol === "function" && _typeof2(Symbol.iterator) === "symbol") {
+    _typeof = function _typeof(obj) {
+      return _typeof2(obj);
+    };
+  } else {
+    _typeof = function _typeof(obj) {
+      return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : _typeof2(obj);
+    };
+  }
+
+  return _typeof(obj);
+}
+
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
+function _possibleConstructorReturn(self, call) {
+  if (call && (_typeof(call) === "object" || typeof call === "function")) {
+    return call;
+  }
+
+  return _assertThisInitialized(self);
+}
+
+function _assertThisInitialized(self) {
+  if (self === void 0) {
+    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+  }
+
+  return self;
+}
+
+function _getPrototypeOf(o) {
+  _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
+    return o.__proto__ || Object.getPrototypeOf(o);
+  };
+  return _getPrototypeOf(o);
+}
+
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function");
+  }
+
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      writable: true,
+      configurable: true
+    }
+  });
+  if (superClass) _setPrototypeOf(subClass, superClass);
+}
+
+function _setPrototypeOf(o, p) {
+  _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
+    o.__proto__ = p;
+    return o;
+  };
+
+  return _setPrototypeOf(o, p);
+}
+
+var Two =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(Two, _React$Component);
+
+  function Two() {
+    _classCallCheck(this, Two);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Two).apply(this, arguments));
+  }
+
+  _createClass(Two, [{
+    key: "render",
+    value: function render() {
+      return _react.default.createElement("div", null, _react.default.createElement("h1", null, "PAGE TWO"));
+    }
+  }]);
+
+  return Two;
+}(_react.default.Component);
+
+var _default = Two;
 exports.default = _default;
 
--- server/server.js
+++ server/server.js
@@ -1,75 +1,172 @@
 const express = require('express');
 const app = new express();
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 
-const React = require('react')
-const reactDomServer = require("react-dom/server");
+const React = require('react');
+const reactDomServer = require('react-dom/server');
 const renderToString = reactDomServer.renderToString;
-import { StaticRouter } from "react-router-dom";
+import { StaticRouter } from 'react-router-dom';
 
 const { exec } = require('child_process');
-const appDir = process.argv[3]
+const appDir = process.argv[2];
 const ribbitConfig = require(path.join(appDir, '/ribbit.config.js'));
-let reactApp;
 
-const babelChild = exec(`npx babel ${appDir}/${ribbitConfig.appFile}`,
-    {
-        cwd: appDir,
-    },
-    () => {
-        console.log('Finished building file.')
+// process.chdir(appDir);
+// import ReactApp from '/home/marlon/Desktop/webpack-games/src/App.js';
+
+const babelParser = require('@babel/parser');
+const babelTraverse = require('@babel/traverse');
+const { transformFromAst } = require('@babel/core');
+
+let ReactApp;
+const AppPath = '/home/marlon/Desktop/webpack-games/src/App.js';
+
+function createAsset(fileName) {
+  let getData = fs.readFileSync(AppPath, 'utf-8');
+
+  let ast = babelParser.parse(getData, {
+    sourceType: 'module'
+  });
+
+  const dependencies = [];
+
+  let generateDependencies = babelTraverse.default(ast, {
+    ImportDeclaration: ({ node }) => {
+      dependencies.push(node.source.value);
     }
-);
+  });
 
-babelChild.stdout.on('data', (data) => {
-    process.stdout.write(data)
-    let writeStream = fs.createWriteStream('ForeignApp.js');
-    writeStream.write(data)
-    writeStream.on('finish', () => {
-        console.log('Finished writing file.')
+  let { code } = transformFromAst(ast, null, {
+    presets: ['@babel/env', '@babel/react'] //applying the presets it means converting to es5 code
+  });
+
+  return {
+    fileName,
+    dependencies,
+    code
+  };
+}
+
+function dependancyGraph(entry) {
+  const initialAsset = createAsset(entry);
+  //collecting all assets
+  const assets = [initialAsset];
+
+  for (const asset of assets) {
+    const dirname = path.dirname(asset.fileName);
+
+    asset.dependencies.forEach(relativePath => {
+      // getting the extension name of file example .js
+      const extname = path.extname(asset.fileName);
+
+      //generating the absolute path
+      const absolutePath = path.join(dirname, relativePath + extname);
+      const childAsset = createAsset(absolutePath);
+      childAsset.fileName = relativePath + extname;
+      assets.push(childAsset);
     });
+  }
+
+  return assets;
+}
+
+function bundle(graph) {
+  let modules = '';
+
+  // for each module we are creating key-value pairs where
+  // the key is the filename of that module and value is its code.
+
+  graph.forEach(mod => {
+    modules += `${JSON.stringify(mod.fileName.replace(/.js$/gi, ''))}: [
+        function ( module, exports,require) {
+          ${mod.code}
+        }
+      ],`;
+  });
+
+  // final thing is we are creating an Immediately invoking function expression
+  //or IIFE where that function accepts the modules in its parameter.
+
+  var result = `(function (modules) {
+      function require(name) {
+        const [fn] = modules[name];
+        const module={},exports={};
+        fn(module, exports,(name)=>require(name));
+        return exports;
+      }
+      require("${AppPath}");
+    })({${modules}})`;
+
+  return result; //finally we are returning the IIFE with modules
+}
+
+const graph = dependancyGraph(AppPath);
+
+fs.appendFile('transpiled.js', bundle(graph), err => {
+  if (err) throw err;
+  console.log('transpiled.js created');
 });
 
-babelChild.stderr.on('data', (data) => {
-    process.stdout.write(data)
-});
+// console.log(code);
 
-babelChild.stderr.on('exit', (data) => {
-    process.stdout.write('im done')
-});
+// const babelChild = exec(
+//   `npx babel ${appDir}/${ribbitConfig.appFile}`,
+//   {
+//     cwd: appDir
+//   },
+//   () => {
+//     console.log('Finished building file.');
+//     const reactApp = require('../ForeignApp.js');
+//   }
+// );
 
+// babelChild.stdout.on('data', data => {
+//   process.stdout.write(data);
+//   let writeStream = fs.createWriteStream('ForeignApp.js');
+//   writeStream.write(data);
+
+//   writeStream.on('finish', () => {
+//     console.log('Finished writing file.');
+//   });
+// });
+
+// babelChild.stderr.on('data', data => {
+//   process.stdout.write('Error in writing file: ', data);
+// });
+
+// babelChild.stderr.on('exit', data => {
+//   process.stdout.write('Error writing file, exiting: ', data);
+// });
 
 app.get('/', (req, res) => {
-    const context = {};
-    const url = req.url
-    const jsx = (
-        <StaticRouter context={context} location={url}>
-            <App />
-        </StaticRouter>
-    );
-    const reactDom = renderToString(jsx);
+  const context = {};
+  const url = req.url;
+  const jsx = (
+    <StaticRouter context={context} location={url}>
+      <App />
+    </StaticRouter>
+  );
+  const reactDom = renderToString(jsx);
 
-    const html = htmlTemplate(reactDom);
+  const html = htmlTemplate(reactDom);
 
-    console.log('====')
-    console.log(html)
-    console.log('====')
+  console.log('====');
+  console.log(html);
+  console.log('====');
 
-    const file = url.replace(/\//g, 'x');
+  const file = url.replace(/\//g, 'x');
 
-    fs.writeFile(`${file}.html`, html, (err) => {
-        if (err) throw err;
-        res.sendFile(path.join(__dirname + `/../${file}.html`));
-
-    })
+  fs.writeFile(`${file}.html`, html, err => {
+    if (err) throw err;
+    res.sendFile(path.join(__dirname + `/../${file}.html`));
+  });
 });
-
 
 app.use(express.static(ribbitConfig.buildFolder));
 
 function htmlTemplate(reactDom) {
-    return `
+  return `
         <!DOCTYPE html>
         <html>
         <head>
@@ -78,18 +175,36 @@ function htmlTemplate(reactDom) {
         </head>
 
         <body>
-            <div id="app">${ reactDom}</div>
+            <div id="app">${reactDom}</div>
             <script src="./dist/main.js"></script>
         </body>
         </html>
     `;
 }
 
-
 app.get('/', (req, res) => {
-    res.send('ribbit')
-})
-
-app.listen(4000, () => {
-    console.log('listening on port 4000');
+  res.send('ribbit');
 });
+
+// app.listen(4000, () => {
+//     console.log('listening on port 4000');
+// });
+
+// let writeStream = fs.createWriteStream('transpiled.js');
+// writeStream.write(code);
+
+// writeStream.on('finish', () => {
+//   console.log('Finished writing file.');
+//   ReactApp = require('transpiled.js');
+//   makeJsx(ReactApp);
+// });
+
+// function makeJsx(App) {
+//   const jsx = (
+//     <StaticRouter context={context} location={url}>
+//       <App />
+//     </StaticRouter>
+//   );
+
+//   console.log('================', jsx);
+// }
