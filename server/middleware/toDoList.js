320b004d8e53e7271b83b2d33439121a58817fbc
Raphael Ram <ramraphael@gmail.com>
2019-02-04 05:06:31 -0500

    Delete todo list

--- server/middleware/toDoList.js
+++
@@ -1,6 +0,0 @@
-- [x] Get template directory from ribbit config
-- [x] Get template file name for route from ribbit routes
-- [x] Get route name from req.url
-- [] Import template file as a string and save it to a variable
-- [] We need to import exported context object from each page component
-	(directory name from appRoot in ribbit config, file name from ribbit routes)
